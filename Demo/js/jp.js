e3143ac8caeafdce394cf63032341267f928394d
jinx <1002877868@qq.com>
2022-06-08 11:59:09 +0800

    7

---
+++ Demo/js/jp.js
@@ -0,0 +1,9 @@
+$('span').click(function() {
+	$('.obx').css("display", "block");
+	$('.box4').css("display", "block");
+});
+$('.qd').click(function() {
+	$('.obx').css("display", "none");
+	$('.box4').css("display", "none");
+	$('.box5').css("display", "none");
+})
